a94d792bb595b805313d49e8bf036876c370e6c5
Anna Vaskovska <annavaskov@gmail.com>
2019-03-09 20:04:49 +0200

    table full

--- src/sections/we-offer/WeOffer.js
+++ src/sections/we-offer/WeOffer.js
@@ -26,7 +26,7 @@ class WeOffer extends React.Component {
                         <div className='full-cont'>
                             <div className='full-header'>Пробный</div>
                             <div className='full-price'>$0</div>
-                            <div className='full-duration'>14 дней</div>
+                            <div className='full-duration'>в месяц</div>
                         </div>
                     </Col>
                 </Row>
@@ -144,6 +144,255 @@ class WeOffer extends React.Component {
                     <Col md={{ span: 3 }}>
                         <div className='cell-regular-full m-auto d-flex justify-content-center align-items-center'>
                             4
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='row-header'>
+                    <Col md={{ span: 6 }}>
+                        <div className='cell-full-title'>
+                            Автодайлер
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-header-trial'>
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-header-full'>
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='row-header'>
+                    <Col md={{ span: 6 }}>
+                        <div className='cell-full-title'>
+                            Автоинформатор
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-header-trial'>
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-header-full'>
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='row-header'>
+                    <Col md={{ span: 6 }}>
+                        <div className='cell-full-title'>
+                            Автоопросник
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-header-trial'>
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-header-full'>
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='row-header'>
+                    <Col md={{ span: 6 }}>
+                        <div className='cell-full-title'>
+                            CRM
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-header-trial'>
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-header-full'>
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='row-header'>
+                    <Col md={{ span: 6 }}>
+                        <div className='cell-full-title'>
+                            Техническая поддержка
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-header-trial'>
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-header-full'>
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='row-regular'>
+                    <Col md={{ span: 6 }}>
+                        <div className='cell-regular-title'>
+                            Бесплатная консультация по вопросам телефонии
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-trial m-auto d-flex justify-content-center align-items-center'>
+                            9
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-full m-auto d-flex justify-content-center align-items-center'>
+                            4
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='row-regular'>
+                    <Col md={{ span: 6 }}>
+                        <div className='cell-regular-title'>
+                            Дополнительные технические настройки системы
+                            согласно техническому заданию заказчика
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-trial m-auto d-flex justify-content-center align-items-center'>
+                            9
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-full m-auto d-flex justify-content-center align-items-center'>
+                            4
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='row-regular'>
+                    <Col md={{ span: 6 }}>
+                        <div className='cell-regular-title'>
+                            Техническая поддержка
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-trial m-auto d-flex justify-content-center align-items-center'>
+                            9
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-full m-auto d-flex justify-content-center align-items-center'>
+                            4
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='row-regular'>
+                    <Col md={{ span: 6 }}>
+                        <div className='cell-regular-title'>
+                            Консультации, ответим в течение
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-trial m-auto d-flex justify-content-center align-items-center'>
+                            9
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-full m-auto d-flex justify-content-center align-items-center'>
+                            4
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='row-regular'>
+                    <Col md={{ span: 6 }}>
+                        <div className='cell-regular-title'>
+                            Время реагирование на аварию
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-trial m-auto d-flex justify-content-center align-items-center'>
+                            9
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-full m-auto d-flex justify-content-center align-items-center'>
+                            4
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='row-regular'>
+                    <Col md={{ span: 6 }}>
+                        <div className='cell-regular-title'>
+                            Время реагирования на критический вопрос
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-trial m-auto d-flex justify-content-center align-items-center'>
+                            9
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-full m-auto d-flex justify-content-center align-items-center'>
+                            4
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='row-regular'>
+                    <Col md={{ span: 6 }}>
+                        <div className='cell-regular-title'>
+                            Выезд специалиста
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-trial m-auto d-flex justify-content-center align-items-center'>
+                            9
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-full m-auto d-flex justify-content-center align-items-center'>
+                            4
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='row-regular'>
+                    <Col md={{ span: 6 }}>
+                        <div className='cell-regular-title'>
+                            Мониторинг работы оборудования заказчика, ICMP
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-trial m-auto d-flex justify-content-center align-items-center'>
+                            9
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-full m-auto d-flex justify-content-center align-items-center'>
+                            4
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='row-regular'>
+                    <Col md={{ span: 6 }}>
+                        <div className='cell-regular-title'>
+                            Возможность переноса сервиса на оборудование
+                            заказчика
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-trial m-auto d-flex justify-content-center align-items-center'>
+                            9
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-regular-full m-auto d-flex justify-content-center align-items-center'>
+                            4
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='row-total'>
+                    <Col md={{ span: 6 }}>
+                        <div className='cell-total-title'>
+                            ИТОГ
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-total-trial m-auto d-flex flex-column'>
+                            <div className='total-price'>$0</div>
+                            <div className='total-duration'>14 дней</div>
+                        </div>
+                    </Col>
+                    <Col md={{ span: 3 }}>
+                        <div className='cell-total-full m-auto d-flex flex-column'>
+                            <div className='total-price'>$300</div>
+                            <div className='total-duration'>в  месяц</div>
                         </div>
                     </Col>
                 </Row>
