3b85d2978882b27f888317ecee79ca3938f17584
Anna Vaskovska <annavaskov@gmail.com>
2019-03-09 21:16:55 +0200

    nav

--- src/sections/nav/Nav.js
+++ src/sections/nav/Nav.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Button from 'react-bootstrap/Button';
 import './styles.css';
 
 const Navigation = () => (
-    <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">VOIPPLUS</Navbar.Brand>
+    <Navbar expand="lg" className='voip-nav'>
+        <Navbar.Brand href="#home">
+            <span className='voip'>VOIP</span>
+            <span className='plus'>PLUS</span>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="d-flex justify-content-end">
-                <Nav.Link className='p-2' href="#home">О нас</Nav.Link>
-                <Nav.Link className='p-2' href="#link">Продукты</Nav.Link>
-                <Nav.Link className='p-2' href="#link">Стоимость звонков</Nav.Link>
-                <Nav.Link className='p-2' href="#link">Контакты</Nav.Link>
-                <Button variant="outline-success">Купить</Button>
+                <Nav.Link className='voip-nav-link' href="#home">О нас</Nav.Link>
+                <Nav.Link className='voip-nav-link' href="#link">Продукты</Nav.Link>
+                <Nav.Link className='voip-nav-link' href="#link">Стоимость звонков</Nav.Link>
+                <Nav.Link className='voip-nav-link' href="#link">Контакты</Nav.Link>
+                <button type='button' className='btn-outline-try btn-outline-try'>Купить</button>
             </Nav>
         </Navbar.Collapse>
     </Navbar>
